3f6e562741552a3afc5187efe85ab4c309352860
YOON <128573948+YoonChaeRim@users.noreply.github.com>
2025-03-14 10:36:53 +0900

    Update JJ_백준문제풀이.js

--- JJ_백준문제풀이.js
+++ JJ_백준문제풀이.js
@@ -1,5 +1,11 @@
+//2557번
 const fs = require('fs'); //node.js 에서 시작을 알려주는 코드드
-const input = fs.readFileSync('/dev/stdin').toString().split('');
+
+console.log("Hello World!"); //출력 
+
+//1000번
+const fs = require('fs'); //node.js 에서 시작을 알려주는 코드드
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
 /* Sync : 동기화 
 /dev/stdin : 입력 받기 ( 파이썬에서 input 역할 ) */
 
@@ -7,3 +13,15 @@ const A = parseInt(input[0]); //parseInt() : 정수형으로 변환
 const B = parseInt(input[1]);
 
 console.log(A+B); //출력 
+
+//1001번
+const fs = require('fs'); //node.js 에서 시작을 알려주는 코드드
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+/* Sync : 동기화 
+/dev/stdin : 입력 받기 ( 파이썬에서 input 역할 ) */
+
+const A = parseInt(input[0]); //parseInt() : 정수형으로 변환
+const B = parseInt(input[1]);
+
+console.log(A-B); //출력 
+
