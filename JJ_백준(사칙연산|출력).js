0e29344b60b64c0e0d9910d1730899c4e77c36a9
YOON <128573948+YoonChaeRim@users.noreply.github.com>
2025-03-15 14:01:28 +0900

    Update and rename JJ_백준문제풀이.js to JJ_백준(사칙연산|출력).js

--- JJ_백준(사칙연산|출력).js
+++ JJ_백준(사칙연산|출력).js
@@ -72,3 +72,66 @@ const fs = require('fs');
 const input = fs.readFileSync('/dev/stdin').toString().trim();
 //trim : 앞뒤 공백 제거
 console.log(input,"??!"); //출력 
+
+
+//18108번
+const fs = require('fs')
+const input = fs.readFileSync('/dev/stdin');
+
+console.log(input-543);
+
+
+//10430번
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const C = parseInt(input[2]);
+
+console.log((A+B)%C);
+console.log(((A%C)+(B%C))%C);
+console.log((A*B)%C);
+console.log(((A%C)*(B%C))%C);
+
+
+//2588번
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split('\n');
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const B1 = Math.floor(B/100); //백의자리
+const B2 = Math.floor((B%100)/10); //십의자리
+const B3 = Math.floor(B%10); //일의자리
+
+console.log(A*B3);
+console.log(A*B2);
+console.log(A*B1);
+console.log(A*B);
+
+
+//11382번
+const fs = require('fs');
+const input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+const A = parseInt(input[0]);
+const B = parseInt(input[1]);
+const C = parseInt(input[2]);
+
+console.log(A+B+C);
+
+
+//10171번
+console.log("\\    /\\");
+console.log(" )  ( ')");
+console.log("(  /  )");
+console.log(" \\(__)|");
+
+
+//107172번
+console.log("|\\_/|");
+console.log("|q p|   /}");
+console.log("( 0 )\"\"\"\\");
+console.log("|\"^\"`    |");
+console.log("||_/=\\\\__|");
