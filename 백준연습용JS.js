1f656cd815f9778edee87e5e5f53aba8302d103f
YOON <128573948+YoonChaeRim@users.noreply.github.com>
2025-03-15 14:29:46 +0900

    Delete 백준연습용JS.js

--- 백준연습용JS.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const input = fs.readFileSync('/dev/stdin').toString().split(' ');
-
-const A = parseInt(input[0]);
-const B = parseInt(input[1]);
-const C = parseInt(input[2]);
-
-console.log(A+B+C);
