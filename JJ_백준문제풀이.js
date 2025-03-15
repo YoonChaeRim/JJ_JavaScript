434a54026d1556325ba136f82f7ed3be827b0ac5
ChaeRim <dbscofla3@gmail.com>
2025-03-15 14:32:14 +0900

    아직필요없어서 삭제제

--- JJ_백준문제풀이.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs'); //node.js 에서 시작을 알려주는 코드드
-const input = fs.readFileSync('/dev/stdin').toString().split('');
-/* Sync : 동기화 
-/dev/stdin : 입력 받기 ( 파이썬에서 input 역할 ) */
-
-const A = parseInt(input[0]); //parseInt() : 정수형으로 변환
-const B = parseInt(input[1]);
-
-console.log(A+B); //출력 
